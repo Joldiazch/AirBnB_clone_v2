85f9b96086666c0073067e788e6cf5c4e77becab
joldiazch <jluis.diaz@udea.edu.co>
2020-04-01 19:54:29 +0000

    change the name attribute for managmet in the db

--- models/state.py
+++ models/state.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, DateTime
+from models import storage
+from models.city import City
 
 
 class State(BaseModel):
@@ -8,4 +11,15 @@ class State(BaseModel):
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City",
+        backref='state',
+        cascade="all, delete"
+    )
+
+    @property
+    def cities(self):
+        all_cities = storage.all(City)
+        return [city for city in all_cities if city['state_id'] == self.id]
