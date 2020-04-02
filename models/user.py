97f2b33db278ae4cc3f85330370b48bc7f857f46
joldiazch <jluis.diaz@udea.edu.co>
2020-04-02 19:12:18 +0000

    update user add relasionship with places table

--- models/user.py
+++ models/user.py
@@ -2,6 +2,9 @@
 """This is the user class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from models.place import Place
+
 
 
 class User(BaseModel, Base):
@@ -17,3 +20,9 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship(
+        "Place",
+        backref='users',
+        cascade="all, delete"
+    )
