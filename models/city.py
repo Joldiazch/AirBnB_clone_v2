8b1f53832756bfdea1daa8292beb28d2660cbb5a
joldiazch <jluis.diaz@udea.edu.co>
2020-04-02 14:44:12 +0000

    fix pep8

--- models/city.py
+++ models/city.py
@@ -4,13 +4,18 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Column, Integer, String, ForeignKey
 
+
 class City(BaseModel, Base):
     """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
-    __tablename__= 'cities'
+    __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), unique=True, nullable=False)
-
+    state_id = Column(
+        String(60),
+        ForeignKey('states.id'),
+        unique=True,
+        nullable=False
+    )
