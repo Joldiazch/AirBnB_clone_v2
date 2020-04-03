d1d98ee5a2be10f6452882e5ec8d3b2794678058
joldiazch <jluis.diaz@udea.edu.co>
2020-04-03 01:13:06 +0000

    add task 10 update

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 class Amenity(BaseModel):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
-    name = ""
+
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Review", backref="user", cascade="all, delete")
+
+    amenities = relationship(
+        'Amenity',
+        secondary=place_amenity,
+        viewonly=False,
+        back_populates='place_amenities'
+    )
