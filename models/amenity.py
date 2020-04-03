511b134682c5629f1a08572533635315f90a02f6
joldiazch <jluis.diaz@udea.edu.co>
2020-04-03 02:06:38 +0000

    modificate pepe8 requeriment

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
-class Amenity(BaseModel):
+
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
@@ -11,7 +12,11 @@ class Amenity(BaseModel):
 
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Review", backref="user", cascade="all, delete")
+    place_amenities = relationship(
+        "Review",
+        backref="user",
+        cascade="all, delete",
+    )
 
     amenities = relationship(
         'Amenity',
