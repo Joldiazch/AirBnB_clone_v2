82d7d22635c2fdbaf0f51fe4d13acd5cbc165595
joldiazch <jluis.diaz@udea.edu.co>
2020-04-09 19:21:33 +0000

    modificate sed command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,13 +8,8 @@ sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
-
-sed -i "s/root \/var\/www\/html;/root \/data\/web_static\/current\/;/g" /etc/nginx/sites-enabled/default
-sed -i "/root \/data\/web_static\/current\/;/a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
-
+echo " Holberton School " | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-
+sed -i "43 a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
