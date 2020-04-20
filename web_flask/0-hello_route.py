e30b09e2a12ca12cdbd007d0664c7656964e8ba2
joldiazch <jluis.diaz@udea.edu.co>
2020-04-20 15:09:00 +0000

    that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" hello world with flask framework """
+# import flask
+from flask import Flask
+
+
+app = Flask(__name__)
+# strict_slashes allow that this route work with /my_route and /my_route/
+@app.route('/', strict_slashes=False)
+def my_function():
+    """ return Hello HBNB """
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
