e3e5ae00332fd21222c5cab84b4d3b5acdddab11
joldiazch <jluis.diaz@udea.edu.co>
2020-04-22 17:17:52 +0000

    modificate sort method and add teardown

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,20 +8,18 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown_db(self):
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def list_of_states():
     states = storage.all(State).values()
-    list_states = [{'name': state.name, 'id': state.id} for state in states]
-    list_states = sorted(list_states, key=lambda i: i['name'])
     return render_template(
         '7-states_list.html',
-        states=list_states
+        states=states
     )
-
-
-@app.teardown_appcontext
-def teardown_db():
-    storage.close()
 
 
 if __name__ == "__main__":
