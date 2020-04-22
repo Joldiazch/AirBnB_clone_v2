c0a2810619b5dde3b67ca8507fbb71ff6fe34d07
joldiazch <jluis.diaz@udea.edu.co>
2020-04-22 17:31:56 +0000

    modificate position of values method

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,7 +15,7 @@ def teardown_db(self):
 
 @app.route('/states_list', strict_slashes=False)
 def list_of_states():
-    states = storage.all(State).values()
+    states = storage.all(State)
     return render_template(
         '7-states_list.html',
         states=states
